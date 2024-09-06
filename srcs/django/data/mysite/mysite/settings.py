2dbf96e879337cd04634814f5642f821655e780d
Floperatok <floperatox@gmail.com>
2024-09-06 18:49:13 +0200

    connection between django and postgresql | volumes suppression

--- srcs/django/data/mysite/mysite/settings.py
+++ srcs/django/data/mysite/mysite/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ['DB_HOST'],
+        "NAME": os.environ['DB_NAME'],
+        "USER": os.environ['DB_USER'],
+        "PASSWORD": os.environ['DB_PASS'],
+        "POST": '5432',
     }
 }
 
--- srcs/django/tools/django_setup.sh
+++ srcs/django/tools/django_setup.sh
@@ -4,4 +4,6 @@ if [ -z "$DJANGO_SECRETKEY" ]; then
 	export DJANGO_SECRETKEY=$(head -c 50 /dev/random | base64)
 fi
 
+env/bin/python mysite/manage.py migrate
+
 env/bin/python mysite/manage.py runserver 0.0.0.0:80 
